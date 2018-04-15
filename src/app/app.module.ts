e71f6640d6ed6e7014faa85e9996d478167005fd
Brandon McFarland <branmcf@gmail.com>
2018-04-15 18:34:50 +0200

    Added landing, signup, and login components. Added router outlet to app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
+const appRoutes: Routes = [
+  { path: '/', component: LandingComponent },
+  { path: '/login', component: LoginComponent },
+  { path: '/signup', component: SignupComponent },
+  { path: '/home', component: HomeComponent },
+  { path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    SettingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes), 
+    FormsModule, 
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
