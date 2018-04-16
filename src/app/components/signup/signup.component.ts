881459ab0114cf0307d1962f3bfb2109b65bebed
Brandon McFarland <branmcf@gmail.com>
2018-04-16 17:49:36 +0200

    Added html/styles to landing, login, and signup components. Added basic data binding for login and  signup.

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  public email: string;
+  public password1: string;
+  public password2: string;
+  public accountType: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public submitSignup() {
+    console.log(this.email);
+    console.log(this.password1);
+    console.log(this.password2);
+    console.log(this.accountType);
   }
 
 }
