0fabb25b9dc9cfc4ca8ad3746408ecaf588076a6
Brandon McFarland <branmcf@gmail.com>
2018-05-01 09:12:53 -0500

    switched to production api endpoints

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -11,59 +11,94 @@ export class ApiService {
     this.touchBase();
   }
 
-  touchBase() : Observable<any> {
-    // return this.http.get('https://blooming-waters-65278.herokuapp.com/');
-    return this.http.get('http://localhost:5000/');
+  // LOCAL TESTING
+  // touchBase() : Observable<any> {
+  //   return this.http.get('http://localhost:5000/');
 
+  // }
+
+  // addUser(formData) : Observable<any> {
+  //   return this.http.post('http://localhost:5000/signup', formData);
+  // }
+
+  // loginUser(formData) : Observable<any> {
+  //   return this.http.post('http://localhost:5000/login', formData);
+  // }
+
+  // addRequest(formData) : Observable<any> {
+  //   return this.http.post('http://localhost:5000/addRequest', formData);
+  // }
+
+  // getAllRequests() : Observable<any> {
+  //   return this.http.get('http://localhost:5000/allRequests');
+  // }
+
+  // getAllOpenRequests() : Observable<any> {
+  //   return this.http.get('http://localhost:5000/allOpenRequests');
+  // }
+
+  // deleteRequestById(reqId) : Observable<any> {
+  //   return this.http.post('http://localhost:5000/deteteRequest', reqId);
+  // }
+
+  // getAllRequestsByUserId(paramId) : Observable<any> {
+  //   return this.http.post('http://localhost:5000/allRequestsByUserId', paramId);
+  // }
+
+  // getUserById(paramId) : Observable<any> {
+  //   return this.http.post('http://localhost:5000/oneUser', paramId);
+  // }
+
+  // getRequestById(paramId) : Observable<any> {
+  //   return this.http.post('http://localhost:5000/getRequestById', paramId);
+  // }
+
+  // getBidsForRequest(paramId) : Observable<any> {
+  //   return this.http.post('http://localhost:5000/getBidsForRequest', paramId);
+  // }
+
+  // PRODUCTION ENDPOINTS
+  touchBase() : Observable<any> {
+    return this.http.get('https://blooming-waters-65278.herokuapp.com/');
   }
 
   addUser(formData) : Observable<any> {
-    // return this.http.post('https://blooming-waters-65278.herokuapp.com/signup', formData);
-    return this.http.post('http://localhost:5000/signup', formData);
+    return this.http.post('https://blooming-waters-65278.herokuapp.com/signup', formData);
   }
 
   loginUser(formData) : Observable<any> {
-    // return this.http.post('https://blooming-waters-65278.herokuapp.com/login', formData);
-    return this.http.post('http://localhost:5000/login', formData);
+    return this.http.post('https://blooming-waters-65278.herokuapp.com/login', formData);
   }
 
   addRequest(formData) : Observable<any> {
-    // return this.http.post('https://blooming-waters-65278.herokuapp.com/login', formData);
-    return this.http.post('http://localhost:5000/addRequest', formData);
+    return this.http.post('https://blooming-waters-65278.herokuapp.com/addRequest', formData);
   }
 
   getAllRequests() : Observable<any> {
-    // return this.http.get('https://blooming-waters-65278.herokuapp.com/');
-    return this.http.get('http://localhost:5000/allRequests');
+    return this.http.get('https://blooming-waters-65278.herokuapp.com/allRequests');
   }
 
   getAllOpenRequests() : Observable<any> {
-    // return this.http.get('https://blooming-waters-65278.herokuapp.com/');
-    return this.http.get('http://localhost:5000/allOpenRequests');
+    return this.http.get('https://blooming-waters-65278.herokuapp.com/allOpenRequests');
   }
 
   deleteRequestById(reqId) : Observable<any> {
-    // return this.http.get('https://blooming-waters-65278.herokuapp.com/');
-    return this.http.post('http://localhost:5000/deteteRequest', reqId);
+    return this.http.post('https://blooming-waters-65278.herokuapp.com/deteteRequest', reqId);
   }
 
   getAllRequestsByUserId(paramId) : Observable<any> {
-    // return this.http.get('https://blooming-waters-65278.herokuapp.com/');
-    return this.http.post('http://localhost:5000/allRequestsByUserId', paramId);
+    return this.http.post('https://blooming-waters-65278.herokuapp.com/allRequestsByUserId', paramId);
   }
 
   getUserById(paramId) : Observable<any> {
-    // return this.http.get('https://blooming-waters-65278.herokuapp.com/');
-    return this.http.post('http://localhost:5000/oneUser', paramId);
+    return this.http.post('https://blooming-waters-65278.herokuapp.com/oneUser', paramId);
   }
 
   getRequestById(paramId) : Observable<any> {
-    // return this.http.get('https://blooming-waters-65278.herokuapp.com/');
-    return this.http.post('http://localhost:5000/getRequestById', paramId);
+    return this.http.post('https://blooming-waters-65278.herokuapp.com/getRequestById', paramId);
   }
 
   getBidsForRequest(paramId) : Observable<any> {
-    // return this.http.get('https://blooming-waters-65278.herokuapp.com/');
-    return this.http.post('http://localhost:5000/getBidsForRequest', paramId);
+    return this.http.post('https://blooming-waters-65278.herokuapp.com/getBidsForRequest', paramId);
   }
 }
